5cec8512496d484a94cf729a1f2efce227fbc954
explosivedeer <explosivedeer@gmail.com>
2020-09-26 19:40:48 -0400

    Update ERS_Functions.sql
    postgresql function "login(username,password)" created

--- src/main/resources/ERS_Functions.sql
+++ src/main/resources/ERS_Functions.sql
@@ -9,7 +9,7 @@ declare passed_id integer;
 		where ERS_USERS.ERS_USERNAME = $1
 		into verify;
 			if verify = true then
-				select ERS_USERS_ID from ERS_USERS where ERS_USERS.ERS_USERS = $1 and ERS_USERS.ERS_PASSWORD = $2 
+				select ERS_USERS_ID from ERS_USERS where ERS_USERS.ERS_USERNAME = $1 and ERS_USERS.ERS_PASSWORD = $2 
 				into passed_id;
 				return passed_id;
 			end if;
@@ -17,3 +17,10 @@ declare passed_id integer;
 	end;
 $passed_id$ language plpgsql;
 -- This will verify the login, if verified, it will return the id associated to the account. Otherwise it will return -1, indicating the login was invalid.
+insert into ers_user_roles (user_role) values (1);
+insert into ers_users (ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) values ('testuser1', 'testpassword1', 'testFirstName', 'testLastName', 'testEmail@email.com', 1);
+select * from ers_users;
+
+select * from ers_users where ers_username = 'testuser1';
+
+select * from login('testuser1', 'testpassword1');
