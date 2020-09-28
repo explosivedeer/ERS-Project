d03cb2a2c5db7748e553886be317f60fe1afd884
explosivedeer <explosivedeer@gmail.com>
2020-09-28 16:26:46 -0400

    Create Reimbursement.java
    model for Reimbursement table

---
+++ src/main/java/com/ers/model/Reimbursement.java
@@ -0,0 +1,127 @@
+package com.ers.model;
+
+public class Reimbursement {
+
+	private int id;
+	private int amount;
+	private String submitted;
+	private String resolved;
+	private String description;
+	// TODO: field for BLOB "REIMB_RECEIPT"
+	private int resolver;
+
+	// pass to database
+	private int author;
+	private int statusid;
+	private int typeid;
+	
+	
+	public Reimbursement(int amount, String submitted, String description, int author) {
+		super();
+		this.amount = amount;
+		this.submitted = submitted;
+		this.description = description;
+		this.author = author;
+	}
+
+	public Reimbursement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getSubmitted() {
+		return submitted;
+	}
+
+	public void setSubmitted(String submitted) {
+		this.submitted = submitted;
+	}
+
+	public String getResolved() {
+		return resolved;
+	}
+
+	public void setResolved(String resolved) {
+		this.resolved = resolved;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getResolver() {
+		return resolver;
+	}
+
+	public void setResolver(int resolver) {
+		this.resolver = resolver;
+	}
+
+	public int getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(int author) {
+		this.author = author;
+	}
+
+	public int getStatusid() {
+		return statusid;
+	}
+
+	public void setStatusid(int statusid) {
+		this.statusid = statusid;
+	}
+
+	public int getTypeid() {
+		return typeid;
+	}
+
+	public void setTypeid(int typeid) {
+		this.typeid = typeid;
+	}
+
+	@Override
+	public String toString() {
+		return "{ \"reimbursement\":{\"id\":"+id+", \"amount\": \""+amount+"\", \"submitted\": \""+submitted+"\", \"description\": \""+description+"\"}}";
+	}
+	
+	// ie from user "{ \"user\":{\"id\":"+id+", \"username\": \""+username+"\", \"password\":\""+password+"\"}}";
+	//This is formatting for JSON
+	
+		/* Example JSON
+		 * 
+		 * {	
+		 * 		"reimbursement":{
+		 * 			"id":1,
+		 * 			"amount":0,
+		 * 			"submitted":"tom",
+		 * 			"description":"pass",
+		 * 			"author":"author"
+		 * 		}
+		 * }
+		 * 
+		 */
+
+	
+}
