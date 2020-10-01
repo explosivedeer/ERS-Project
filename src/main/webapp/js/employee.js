6d96ba3eddef77463e4b47c1204f31633d4e015f
“Francisco <YOUR@EMAIL.com>
2020-10-01 16:59:31 -0400

    removed button

--- src/main/webapp/js/employee.js
+++ src/main/webapp/js/employee.js
@@ -1,33 +1,4 @@
-function closeMe(){
-      // Find the element
-   		x=document.getElementById("formDisplay");
-       //Option 1: Change the style attribute directly
-   		x.style.display="none";
-  
-	}
 
-function openMe(){
-      // Find the element
-   		x=document.getElementById("formDisplay");
-      //Option 1: Change the style attribute directly
-   		 x.style.display="block";
- 
-	}
-function closeTable(){
-      // Find the element
-   		x=document.getElementById("tableDisplay");
-       //Option 1: Change the style attribute directly
-   		x.style.display="none";
-  
-	}
-
-function openTable(){
-      // Find the element
-   		x=document.getElementById("tableDisplay");
-      //Option 1: Change the style attribute directly
-   		 x.style.display="block";
- 
-	}
 function closeMe2(){
       // Find the element
    		x=document.getElementById("formDisplay2");
